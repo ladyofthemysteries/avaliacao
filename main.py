20a7383fd37ba754d2e5954c9102ce4132235b3e
cynarapires <cynarapires127@gmail.com>
2025-05-31 14:00:24 -0300

    primeiro

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
-from rotas_tarefas import roteador_tarefas
 from rotas_auth import router as roteador_auth
+from rotas_tarefas import roteador_tarefas
+
 
 
 app = FastAPI()
@@ -8,5 +9,5 @@ app = FastAPI()
 # As rotas GET /tasks, POST /tasks, PUT /tasks/{id}
 app.include_router(roteador_tarefas)
 
-# Rotas de Autenticaco POST /auth/signup, POST /auth/login, GET /auth/me
+# Rotas de Autenticaco POST /auth/signup, POST /auth/login, GET /auth/m
 app.include_router(roteador_auth)
